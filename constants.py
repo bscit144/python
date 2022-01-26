25380327805365718bd67027bf5de98d87655db9
bscit144 <98459640+bscit144@users.noreply.github.com>
2022-01-26 16:26:01 +0300

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,60 @@
+#string
+#constants
+name=input("Enter your name")
+print(type(name))
+print(name)
+
+#numeric
+meric
+age=int(input ("Enter your age:"))
+print(type(age))
+print(age)
+
+#numeric2
+year=int(input("Enter your year:"))
+print(type(year))
+print(year)
+
+#variables
+x=2
+y=3
+w=(2*x+3*y)
+print(w)
+
+#variables
+x=2
+y=3
+z=20
+w=(2*x+3**y/z-4**1)
+print("operation:")
+print(w)
+
+print(20%6)
+
+#type
+name="brashon"
+print(type(name))
+
+age=20
+print(type(age))
+
+price=100.23
+print(type(price))
+
+
+#concantenation
+str1="hey"
+str2="bro"
+a=str(10)
+
+print(str1+str2)
+print(str1*10)
+print(str1+a)
+
+hours=int(input("Enter hours"))
+print(hours)
+
+rate=float(input("Enter rate"))
+print(rate)
+pay=rate*hours
+print(pay)
