81af269c725d40a99cec93e221fb1062c272f5ff
bscit144 <98459640+bscit144@users.noreply.github.com>
2022-02-11 15:51:43 +0300

    if statement

---
+++ largenumber.py
@@ -0,0 +1,11 @@
+
+A=int(input("Enter firstnumber"))
+B=int(input("Enter secondnumber"))
+C=int(input("Enter secondnumber"))
+if A>B AND A>C:
+  print("largest number is:",A)
+else
+  print("largest number is:",B)
+else
+   print("largest number is:",B)
+  
