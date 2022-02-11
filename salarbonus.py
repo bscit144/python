e7a419ecb1384adcc96715d813b1f6b9732f0cd1
bscit144 <98459640+bscit144@users.noreply.github.com>
2022-02-11 16:27:03 +0300

    if-else statement calculate bonus

---
+++ salarbonus.py
@@ -0,0 +1,10 @@
+salary=int(input("enter salary"))
+years=int(input("enter service years"))
+if year>20
+bonus=20/100*salary
+  print("bonus is:",bonus)
+if years<=20 and years>=10
+  print(not permitted to bonus)
+else
+  bonus=salary*6/100
+  print("bonus is:",bonus)
