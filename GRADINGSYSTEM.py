581ad9958b9605045d766d78008a8f50159d3ba7
bscit144 <98459640+bscit144@users.noreply.github.com>
2022-02-16 16:02:49 +0300

    Add files via upload

---
+++ GRADINGSYSTEM.py
@@ -0,0 +1,17 @@
+grade=int(input("enter grade"))
+
+if grade >= 70 and <= 100:
+
+        print ('A')
+
+    elif grade >= 60 and <= 69:
+
+        print('B')
+
+    elif grade >= 50 and <= 59:
+
+        print('C')
+
+    elif grade >= 40 and <= 49:
+
+        print('D')
