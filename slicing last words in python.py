78a5862b83f523b17f2f1648be7c5c52344a96c7
bscit144 <98459640+bscit144@users.noreply.github.com>
2022-03-07 10:22:56 +0300

    Add files via upload

---
+++ slicing last words in python.py
@@ -0,0 +1,12 @@
+>>> d="ZETECH UNIVERSITY"
+>>> print(d[7:17])
+UNIVERSITY
+>>>
+>>>
+>>> z="brshon"
+>>> print(z[:1])
+b
+
+>>>b="brashon"
+>>>print(b[2:])
+ashon
---
+++ strings lenth and position in python.py
@@ -0,0 +1,13 @@
+
+string positioning and lenth
+
+
+>>> a="onyangobrayan"
+>>> print(a[5])
+g
+
+
+
+>>> x="brayan"
+>>> print(len(x))
+6
