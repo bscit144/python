6e1a2a3e5b3c66a3163a811ca73e99a3666fdcb0
bscit144 <98459640+bscit144@users.noreply.github.com>
2022-03-09 15:56:00 +0300

    Add files via upload

---
+++ Dictionery.py
@@ -0,0 +1,17 @@
+ 
+
+      DICTIONERY
+
+values={1:'brashon',2:'onyango',5:'both names',6:'are mine'}
+>>> values
+{1: 'brashon', 2: 'onyango', 5: 'both names', 6: 'are mine'}
+>>> 5
+5
+>>> values[5]
+'both names'
+>>> values[1]
+'brashon'
+>>> values.get(1)
+'brashon'
+>>> values.get(6)
+'are mine'
